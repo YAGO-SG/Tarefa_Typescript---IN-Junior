4683113b066234a74aee2b7062d6030711377f1a
Yago Santos Gois <yago.sant.gois@gmail.com>
2026-02-11 16:16:07 -0300

    finalizando exercício 1

--- CódigoJS/Exercício1.js
+++ CódigoJS/Exercício1.js
@@ -1,3 +1,7 @@
 "use strict";
-let MyName = "papuda";
-console.log(MyName);
+let total = 100;
+function adicionar(a = 10, b = 20) {
+    return a + b;
+}
+const resultado = adicionar(20, 30);
+console.log(resultado);
--- ExercíciosTS/Exercício1.ts
+++ ExercíciosTS/Exercício1.ts
@@ -1,2 +1,9 @@
-let MyName: string = "papuda";
-console.log(MyName);
+let total = 100;
+
+function adicionar (a = 0, b = 0) {
+
+    return a + b;
+}
+
+const resultado = adicionar(20,30);
+console.log(resultado); 
