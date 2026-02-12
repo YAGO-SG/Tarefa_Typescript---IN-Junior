c3d9fc088221dddc4d1e12efa93b8c65d85db039
Yago Santos Gois <yago.sant.gois@gmail.com>
2026-02-12 10:09:42 -0300

    feat: finalizando exercício 2

--- CódigoJS/Exercício2.js
+++ CódigoJS/Exercício2.js
@@ -1,2 +1,41 @@
 "use strict";
-console.log("Ola! dev");
+function mostrarMensagemStatus(status) {
+    if (status == "sucesso") {
+        console.log(`dados carregados com sucesso`);
+    }
+    else if (status == "erro") {
+        console.log(`erro ao carregar dados`);
+    }
+    else {
+        console.log(`Aguarde... carregando dados`);
+    }
+}
+mostrarMensagemStatus("carregando");
+/*let Lista: number[] = [1, 4, 6, 7, 3, 2, 7];
+console.log(Lista);
+
+interface lista {
+    lista: string;
+    valor: number;
+    produtos: Iprodutos;
+}
+
+interface Iprodutos {
+    nome: 'biscoito' | 'fruta'
+}
+
+interface limpeza extends lista {
+    produto1: string;
+    produto2: string;
+}
+
+
+const mercado: limpeza = {
+    lista: "lista 2",
+    valor: 203,
+    produtos: { nome: "biscoito"},
+    produto1: "lavanda",
+    produto2: "espumante",
+}
+
+console.log(mercado);*/ 
