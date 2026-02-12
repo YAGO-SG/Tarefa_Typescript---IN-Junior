fb046463d2bde11a83f57fd0482b6fde4d699f09
Yago Santos Gois <yago.sant.gois@gmail.com>
2026-02-12 10:14:35 -0300

    style: removendo comentários desnecessários

--- CódigoJS/Exercício2.js
+++ CódigoJS/Exercício2.js
@@ -11,31 +11,3 @@ function mostrarMensagemStatus(status) {
     }
 }
 mostrarMensagemStatus("carregando");
-/*let Lista: number[] = [1, 4, 6, 7, 3, 2, 7];
-console.log(Lista);
-
-interface lista {
-    lista: string;
-    valor: number;
-    produtos: Iprodutos;
-}
-
-interface Iprodutos {
-    nome: 'biscoito' | 'fruta'
-}
-
-interface limpeza extends lista {
-    produto1: string;
-    produto2: string;
-}
-
-
-const mercado: limpeza = {
-    lista: "lista 2",
-    valor: 203,
-    produtos: { nome: "biscoito"},
-    produto1: "lavanda",
-    produto2: "espumante",
-}
-
-console.log(mercado);*/ 
