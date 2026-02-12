57f7aac2c21eae1231c1eff928ae054eedc321c4
Yago Santos Gois <yago.sant.gois@gmail.com>
2026-02-12 11:00:40 -0300

    feat: finalizando exercício 3

--- CódigoJS/Exercício3.js
+++ CódigoJS/Exercício3.js
@@ -1,2 +1,10 @@
 "use strict";
-console.log("amigo, dev");
+function resumirLivro(resumo) {
+    console.log(`O livro '${resumo.titulo}' foi escrito por ${resumo.autor} em ${resumo.anoPublicacao}`);
+}
+let meuLivro = {
+    titulo: "Dom Casmurro",
+    autor: "Machado de Assis",
+    anoPublicacao: 1899,
+};
+resumirLivro(meuLivro);
--- ExercíciosTS/Exercício3.ts
+++ ExercíciosTS/Exercício3.ts
@@ -1,1 +1,18 @@
-console.log("amigo, dev");
+interface Livro {
+    titulo: string;
+    autor: string;
+    anoPublicacao: number;
+}
+
+function resumirLivro (resumo: Livro) {
+    console.log(`O livro '${resumo.titulo}' foi escrito por ${resumo.autor} em ${resumo.anoPublicacao}`)
+}
+
+let meuLivro: Livro = {
+    titulo: "Dom Casmurro",
+    autor: "Machado de Assis",
+    anoPublicacao: 1899,
+}
+
+resumirLivro(meuLivro);
+ 
